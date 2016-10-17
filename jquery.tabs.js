31918da1f9bf00c7294c325ae32e0c3116a75afb
Ian McBurnie <ianmcburnie@hotmail.com>
2016-10-17 15:43:47 -0700

    Minor update for latest roving-tabindex API

--- jquery.tabs.js
+++ jquery.tabs.js
@@ -1,7 +1,7 @@
 /**
 * @file jQuery plugin that creates the basic interactivity for an ARIA tabs widget
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.4.1
+* @version 0.4.2
 * @requires jquery
 * @requires jquery-next-id
 * @requires jquery-roving-tabindex
@@ -72,7 +72,7 @@
             });
 
             // Create a roving tab index on tabs
-            $tablist.rovingTabindex('[role=tab]', {autoWrap: true});
+            $tablist.rovingTabindex('[role=tab]', {autoWrap: true, disableHomeAndEndKeys: true});
 
             $tablist.on('rovingTabindexChange', '[role=tab]', function(e, data) {
                 var $selectedTab = $(this);
